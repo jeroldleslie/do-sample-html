e65b747bd5cfadcde940bdf15eadd4c41732b95c
Peter Jerold Leslie <jeroldleslie@gmail.com>
2022-10-22 04:24:22 +0530

    added files

--- src/js/animation.js
+++ src/js/animation.js
@@ -76,13 +76,6 @@ export const animate = function () {
               door icon will appear. 
           */
 
-      // (A1) GO INTO FULL SCREEN FIRST
-      let de = document.documentElement;
-      if (de.requestFullscreen) { de.requestFullscreen(); }
-      else if (de.mozRequestFullScreen) { de.mozRequestFullScreen(); }
-      else if (de.webkitRequestFullscreen) { de.webkitRequestFullscreen(); }
-      else if (de.msRequestFullscreen) { de.msRequestFullscreen(); }
-
       // (A2) THEN LOCK ORIENTATION
       screen.orientation.lock("landscape");
 
